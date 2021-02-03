ba80d5f16f2220630ccf0368abe88d2dfee04a74
luislvalente <luislvalente@gmail.com>
2021-02-03 17:38:34 +0100

    first commit

---
+++ R/ICs.R
@@ -0,0 +1,41 @@
+
+#LogLik- vector of logliks e.g. LogLik = c(-100.1,-102,-105.2)
+# k - number of parameters e.g. k =c(4,5,3)
+# n - sample size, should be the same as M e.g. n = 1000
+# IC_type - for the weights function only - 'AIC', 'BIC' or 'AICc'
+
+# AIC Akaike Information Criterion
+AIC <- function(LogLik,k){
+  aic <- (2*k)-(2*LogLik)
+  return(aic)
+}
+
+## AIC for small sample sizes
+AICc <- function(LogLik,k,n){
+  aicc <- AIC(LogLik,k)+((2*k*(k+1))/(n-k-1))
+  return(aicc)
+}
+
+# BIC Bayesian Information Criterion
+BIC <- function(LogLik,k,n){
+  bic <- -2*LogLik+k*(log(n)+log(2*pi))
+  return(bic)
+}
+
+
+# IC weights 
+ICweights <- function(LogLik,k,n,IC_type){
+  if(IC_type == 'AIC'){
+    IC <- AIC(LogLik,k)
+  }
+  if(IC_type == 'AICc'){
+    IC <- AICc(LogLik,k,n)
+  }
+  if(IC_type == 'BIC'){
+    IC <- BIC(LogLik,k,n)
+  }
+  bestmodelIC <- min(IC)
+  weights <- exp(-0.5*(IC-bestmodelIC))
+  weights <- weights/sum(weights)
+  return(weights)
+}
